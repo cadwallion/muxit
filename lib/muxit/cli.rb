e52bf15d6068e6253fddc09aef9adf43ffa9c3dc
Andrew Nordman <cadwallion@gmail.com>
2013-04-23 20:36:05 -0500

    Adds tmux_exec to the CLI

--- lib/muxit/cli.rb
+++ lib/muxit/cli.rb
@@ -2,31 +2,36 @@ module Muxit
   class CLI < Thor
     desc 'start NAME', 'Start a named tmux session'
     def start name
-      `tmux new-session -s #{name}`
+      tmux_exec ['tmux', 'new-session', '-s', '#{name}']
     end
 
     desc 'list', 'List all existing tmux sessions'
     def list
-      IO.popen("tmux list-sessions", 'r') do |process|
-        puts process.gets until process.eof?
-      end
+      tmux_exec ['tmux', 'list-sessions']
     end
 
     desc 'attach NAME', 'Attach to NAME session'
     def attach name
-      `tmux attach-session -t #{name}`
+      tmux_exec ['tmux', 'attach-session', '-t', name]
     end
 
     desc 'nuke NAME', 'Destroy a named session'
     def nuke name
-      `tmux kill-session -t #{name}`
+      tmux_exec ['tmux', 'kill-session', '-t', name]
     end
 
     desc 'code NAME', 'opens up a new tmux session with split windows, in the project directory, with an $EDITOR pane'
     def code project
       name = project.split('/').last
       directory = "#{ENV['PROJECTS']}/#{project}"
-      `tmux new-session -s #{name} 'cd #{directory} && #{ENV['EDITOR']}' \\; split-window -h -c #{directory}`
+      tmux_exec "tmux new-session -s #{name} 'cd #{directory} && #{ENV['EDITOR']}' \\; split-window -h -c #{directory}"
+    end
+
+    private
+    def tmux_exec tmux_command
+      IO.popen(tmux_command, 'r') do |process|
+        puts process.gets until process.eof?
+      end
     end
   end
 end
